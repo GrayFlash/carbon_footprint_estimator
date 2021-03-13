5495c402234dc6ec2000177834289881aac228bd
GrayFlash <gk74533@gmail.com>
2021-03-13 11:18:46 +0530

    Flask Setup

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+# Configure application
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    greeting = 'Welcome to My Portfolio Website'
+
+    return render_template('/index.html', greeting=greeting)
