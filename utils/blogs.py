0005cc8307a10ad32fbcada24cc5b42e8757ef6a
Mustafa2856 <56853858+Mustafa2856@users.noreply.github.com>
2021-03-14 10:38:50 +0530

    Update blogs.py

--- utils/blogs.py
+++ utils/blogs.py
@@ -4,7 +4,7 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Mustafa@2856'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'Blogs'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
