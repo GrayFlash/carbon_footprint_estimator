0ab28213c90417e40f0036c3d3e2cd81e1983a84
Mustafa2856 <officewalamustafa@gmail.com>
2021-03-14 13:38:09 +0530

    Blogs db

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, redirect, flash, request
 from utils.dte_scraper import *
 from utils.blogs import *
 from flask import url_for
-
-from utils.db import get_db
 import pandas as pd
 import numpy as np
 # Configure application
@@ -16,7 +14,7 @@ def index():
 @app.route('/fetch_news', methods=['GET','POST'])
 def fetch_news():
     news_list = scrap()
-    print("new_list=> ",news_list)
+    #print("new_list=> ",news_list)
     return render_template('/news_dte.html', news_list=news_list)
 
 @app.route('/contact_us', methods=['POST', 'GET'])
@@ -36,9 +34,24 @@ def about():
     print("Move to about")
     return render_template('/about.html')
     
-@app.route('/blogs',methods=['POST','GET'])
+@app.route('/blogs',methods=['POST', 'GET'])
 def blogs():
+    if request.method == 'POST':
+      author = request.form['author']
+      title = request.form['title']
+      body = request.form['body']
+      if body!='' and title!='':
+        add_blog((title,author,body))
     blog_list = getblogs()
-    print(blog_list)
+    #print(blog_list)
     return render_template('/blogs.html' , blogs=blog_list)
 
+@app.route('/blogs_read',methods=['GET'])
+def blogs_read():
+    id_ = request.args.get('id')
+    blog = getblog(int(id_))
+    return render_template('/blogs_read.html' , blog=blog)
+    
+@app.route('/blog_create',methods=['POST','GET'])
+def blogs_create():
+    return render_template('/blog_create.html')
--- utils/blogs.py
+++ utils/blogs.py
@@ -4,17 +4,30 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Password@123'
 app.config['MYSQL_DATABASE_DB'] = 'Blogs'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
-def addblog():
-  
+def add_blog(data):
+    conn = mysql.connect()
+    cursor =conn.cursor()
+    curr = cursor.execute("SELECT * FROM post")
+    cursor.execute("""INSERT INTO post(id,author_name,title,body) VALUES(%(id)s ,%(author)s ,%(title)s ,%(body)s )"""
+                    ,{'id':int(curr)+1,'author':data[1],'title':data[0],'body':data[2]})
+    conn.commit()
 
 def getblogs():
     conn = mysql.connect()
     cursor =conn.cursor()
     cursor.execute("SELECT * FROM post")
     data = cursor.fetchall()
+    return data
+    
+def getblog(k):
+    conn = mysql.connect()
+    cursor =conn.cursor()
+    k = int(k)
+    cursor.execute("SELECT * FROM post WHERE id = " + str(k))
+    data = cursor.fetchone()
     return data
