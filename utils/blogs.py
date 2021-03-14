bf64e2a54f3a269e097d73592dc44452d0538b8b
Mustafa2856 <officewalamustafa@gmail.com>
2021-03-14 10:44:15 +0530

    blogs read updated

--- utils/blogs.py
+++ utils/blogs.py
@@ -4,13 +4,13 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Mustafa@2856'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'Blogs'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 def addblog():
-  pass
+  
 
 def getblogs():
     conn = mysql.connect()
