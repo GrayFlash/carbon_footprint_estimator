9ff23796692c152bd360a93b195b66e458dcaf10
GrayFlash <gk74533@gmail.com>
2021-03-14 06:07:24 +0530

    Add routes

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 from utils.dte_scraper import *
+from flask import url_for
+
 # Configure application
 app = Flask(__name__)
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -11,4 +14,13 @@ def index():
 def fetch_news():
     news_list = scrap()
     print("new_list=> ",news_list)
-    return render_template('/news.html', news_list=news_list)
+    return render_template('/news_dte.html', news_list=news_list)
+
+@app.route('/contact_us', methods=['POST', 'GET'])
+def contact_us():
+    return render_template('/contact.html')
+
+@app.route('/about', methods=['POST', 'GET'])
+def about():
+    print("Move to about")
+    return render_template('/about.html')
